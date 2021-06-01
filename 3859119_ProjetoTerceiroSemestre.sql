6179d8488ea895a5fff2fa1399f93f80414287b1
bertoli <bert0li@outlook.com.br>
2021-05-31 22:46:28 -0300

    ajustes.

--- 3859119_ProjetoTerceiroSemestre.sql
+++ 3859119_ProjetoTerceiroSemestre.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `db_loja`
+-- Banco de dados: `3859119_ProjetoTerceiroSemestre`
 --
 
 -- --------------------------------------------------------
